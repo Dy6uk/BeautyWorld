960f07aacf04914299b1a23efa4acea492d19286
Dy6uk <dubonosov110@gmail.com>
2022-10-08 17:09:55 +0700

    1

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+const {src, dest} = require('gulp');
+
+function buildSass() {
+    return src('src/scss/**/*.scss')
+        .pipe(sass({ includePaths: ['./node_modules'] }).on('error', sass.logError))
+        .pipe(dest('src/css'))
+        .pipe(dest('dist/css'));
+
+        exports.sass = buildSass;
