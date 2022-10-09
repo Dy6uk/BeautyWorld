84c34fe3549b220344bc16a8d57a04118ba05677
Dy6uk <dubonosov110@gmail.com>
2022-10-09 11:15:39 +0700

    1

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,49 @@ const { src, dest, series, watch, parallel } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const clean = require('gulp-clean');
 const browserSync = require('browser-sync').create();
+const sourcemaps = require('gulp-sourcemaps');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+
+function buildSassDev() {
+    return src('src/scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({ includePaths: ['./node_modules'] }).on('error', sass.logError))
+        .pipe(
+            postcss([
+                autoprefixer({
+                    grid: true,
+                    overrideBrowserslist: ['last 2 versions']
+                }),
+                cssnano()
+            ])
+        )
+        .pipe(sourcemaps.write())
+        .pipe(dest('src/css'))
+        .pipe(dest('dist/css'));        
+}
 
 function buildSass() {
     return src('src/scss/**/*.scss')
         .pipe(sass({ includePaths: ['./node_modules'] }).on('error', sass.logError))
-        .pipe(dest('src/css'))
-        .pipe(dest('dist/css'));
-        .pipe(browserSync.stream());
+        .pipe(
+            postcss([
+                autoprefixer({
+                    grid: true,
+                    overrideBrowserslist: ['last 2 versions']
+                }),
+                cssnano()
+            ])
+        )
+        .pipe(dest('dist/css'))
 }
+
 
 function buildHtml() {
     return src('src/**/*.html').pipe(dest('dist'));
-    .pipe(browserSync.stream());
+    
     
 }
 
@@ -26,7 +57,7 @@ function cleanDist() {
 }
 
 function serve() {
-    watch('src/scss/**/*.scss', buildSass);
+    watch('src/scss/**/*.scss', buildSassDev);
     watch('src/**/*.html', buildHtml);
 }
 
@@ -39,4 +70,4 @@ function createDevServer() {
 
 exports.watch = serve;
 exports.build = series(cleanDist, parallel( buildSass, buildHtml, copy));
-exports.default = series(buildSass, parallel(createDevServer, serve));
+exports.default = series(buildSassDev, parallel(createDevServer, serve));
